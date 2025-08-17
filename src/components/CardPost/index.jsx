8ab314c9ba4baf2322637297f3a63fb9083bfe4e
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-08-17 18:15:24 -0300

    mais sobre mutations

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -32,6 +32,35 @@ export const CardPost = ({ post, highlight, rating, category, isFetching, curren
     }
   })
 
+  const submitCommentMutation = useMutation({
+    mutationFn: (commentData) => {
+      return fetch(`http://localhost:3000/api/comment/${post.id}`, {
+        method: "POST", 
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(commentData)
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["post", post.slug]);
+      queryClient.invalidateQueries(["posts", currentPage]);
+    },
+    onError: (error, variables) => {
+      console.error(
+        `Erro ao salvar o comentário para o slug: ${variables.slug}`,
+        { error }
+      );
+    },
+  })
+
+  const onSubmitComment = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const text = formData.get("text");
+
+    submitCommentMutation.mutate({ id: post.id, text });
+  };
+
   return (
     <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
       <header className={styles.header}>
@@ -63,7 +92,7 @@ export const CardPost = ({ post, highlight, rating, category, isFetching, curren
             <p>{post.likes}</p>
           </form>
           <div>
-            <ModalComment />
+            <ModalComment onSubmit={onSubmitComment}/>
             <p>{post.comments.length}</p>
           </div>
           {rating && (
